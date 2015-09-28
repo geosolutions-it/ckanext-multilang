b052ad3c04c1e36bd777af206bc80d6860913e0d
tobia <tobia.dipisa@geo-solutions.it>
2015-09-28 18:14:13 +0200

    Fixing User controller routes mapping

--- ckanext/multilang/controllers/user.py
+++ ckanext/multilang/controllers/user.py
@@ -1,22 +1,41 @@
-
 import logging
+from urllib import quote
+
+from pylons import config
+
+import ckan.lib.base as base
+import ckan.model as model
+import ckan.lib.helpers as h
 import ckan.authz as authz
 import ckan.logic as logic
-
-import ckan.lib.helpers as h
+import ckan.logic.schema as schema
+import ckan.lib.captcha as captcha
+import ckan.lib.mailer as mailer
+import ckan.lib.navl.dictization_functions as dictization_functions
+import ckan.lib.authenticator as authenticator
+import ckan.plugins as p
 
 from ckan.common import _, c, g, request, response
-from ckan.lib.base import model
+
 from pylons.i18n.translation import get_lang
 from ckanext.multilang.model import PackageMultilang
 
 from ckan.controllers.user import UserController
 
+log = logging.getLogger(__name__)
+
+abort = base.abort
+render = base.render
+
+check_access = logic.check_access
+get_action = logic.get_action
 NotFound = logic.NotFound
 NotAuthorized = logic.NotAuthorized
-get_action = logic.get_action
+ValidationError = logic.ValidationError
+UsernamePasswordError = logic.UsernamePasswordError
 
-log = logging.getLogger(__name__)
+DataError = dictization_functions.DataError
+unflatten = dictization_functions.unflatten
 
 class MultilangUserController(UserController):
     '''
