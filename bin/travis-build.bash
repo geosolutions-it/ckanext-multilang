da43e21b7c1ef07973a3656652601629d0e6ae4f
Cezary Statkiewicz <cezio@thelirium.net>
2018-06-04 18:33:46 +0200

    update travis scripts

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -1,24 +1,60 @@
 #!/bin/bash
 set -e
+set -x
 
 echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
-sudo apt-get update -qq
-sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java
+
+# remove faulty mongodb repo, we don't use it anyway
+sudo rm -f /etc/apt/sources.list.d/mongodb-3.2.list
+sudo add-apt-repository --remove 'http://us-central1.gce.archive.ubuntu.com/ubuntu/ main restricted'
+sudo add-apt-repository --remove 'http://us-central1.gce.archive.ubuntu.com/ubuntu/ universe'
+sudo add-apt-repository --remove 'http://us-central1.gce.archive.ubuntu.com/ubuntu/ multiverse'
+sudo add-apt-repository 'http://archive.ubuntu.com/ubuntu/'
+sudo add-apt-repository 'http://archive.ubuntu.com/ubuntu/ universe'
+sudo add-apt-repository 'http://archive.ubuntu.com/ubuntu/ multiverse'
+sudo apt-get -qq --fix-missing update
+sudo apt-get install solr-jetty libcommons-fileupload-java
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-git checkout release-v2.2
+if [ $CKANVERSION == 'master' ]
+then
+    echo "CKAN version: master"
+else
+    CKAN_TAG=$(git tag | grep ^ckan-$CKANVERSION | sort --version-sort | tail -n 1)
+    git checkout $CKAN_TAG
+    echo "CKAN version: ${CKAN_TAG#ckan-}"
+fi
+
+# Unpin CKAN's psycopg2 dependency get an important bugfix
+# https://stackoverflow.com/questions/47044854/error-installing-psycopg2-2-6-2
+sed -i '/psycopg2/c\psycopg2' requirements.txt
+
 python setup.py develop
+
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
 cd -
 
+echo
+echo "Setting up Solr..."
+printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+
+sudo service jetty restart
+
+echo
 echo "Creating the PostgreSQL user and database..."
+
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
+
+
+echo "Install other libraries required..."
+sudo apt-get install python-dev libxml2-dev libxslt1-dev libgeos-c1
 
 echo "Initialising the database..."
 cd ckan
