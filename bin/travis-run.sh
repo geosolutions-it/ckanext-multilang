e7b49b01ff990bbdcd26eff6a80c876dc7ca157a
Cezary Statkiewicz <cezio@thelirium.net>
2018-06-04 19:24:40 +0200

    test only multilang

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -e
+set -e
+set -x
 
-nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --with-coverage --cover-package=ckanext.multilang --cover-inclusive --cover-erase --cover-tests
+nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --with-coverage --cover-package=ckanext.multilang --cover-inclusive --cover-erase --cover-tests ckanext/multilang
