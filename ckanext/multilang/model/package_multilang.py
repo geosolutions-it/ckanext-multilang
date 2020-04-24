8aeca62ec595ab9c6556260f346a8b5e83406d9f
abulojoshua1 <abulojoshua1@gmail.com>
2020-04-24 04:30:07 +0200

    Add helper functions to filter and save multilingual tags

--- ckanext/multilang/model/package_multilang.py
+++ ckanext/multilang/model/package_multilang.py
@@ -284,6 +284,13 @@ class TagMultilang(DomainObject):
         self.text = text
 
     @classmethod
+    def get_all(cls, tag_name, tag_lang=None):
+        query = meta.Session.query(TagMultilang).filter(TagMultilang.tag_name == tag_name)
+        if tag_lang:
+            query = query.filter(TagMultilang.lang == tag_lang)
+        return query
+
+    @classmethod
     def by_name(self, tag_name, tag_lang, autoflush=True):
         query = meta.Session.query(TagMultilang).filter(TagMultilang.tag_name==tag_name, TagMultilang.lang==tag_lang)
         query = query.autoflush(autoflush)
@@ -313,9 +320,9 @@ class TagMultilang(DomainObject):
     def persist(self, tag, lang):
         session = meta.Session
         try:
-            session.add_all([
+            session.add(
                 TagMultilang(tag_id=tag.get('id'), tag_name=tag.get('name'), lang=lang, text=tag.get('text')),
-            ])
+            )
 
             session.commit()
         except Exception, e:
@@ -344,5 +351,6 @@ class TagMultilang(DomainObject):
 
             log.error('Exception occurred while persisting DB objects: %s', e)
             raise
+
 
 meta.mapper(TagMultilang, tag_multilang_table)
