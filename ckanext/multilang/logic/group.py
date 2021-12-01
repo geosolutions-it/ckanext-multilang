b648fa3384a16ae5eef32709b40a9a6cc0dd5035
haidarCheaito <haidar.ch@live.com>
2021-12-01 13:08:11 +0200

    fix imports

--- ckanext/multilang/logic/group.py
+++ ckanext/multilang/logic/group.py
@@ -1,3 +1,10 @@
+import logging
+
+import ckanext.multilang.helpers as helpers
+from ckanext.multilang.model import GroupMultilang
+
+log = logging.getLogger(__name__)
+
 def delete_multilang_group(entity):
     groups = GroupMultilang.get_for_group_id(entity.id)
     for group in groups:
--- ckanext/multilang/logic/tag.py
+++ ckanext/multilang/logic/tag.py
@@ -1,3 +1,11 @@
+import logging
+
+import ckanext.multilang.helpers as helpers
+from ckanext.multilang.model import TagMultilang
+
+log = logging.getLogger(__name__)
+
+
 def delete_multilang_tag(entity):
     tags = TagMultilang.get_all(entity.name)
     for tag in tags:
